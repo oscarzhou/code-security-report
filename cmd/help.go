74ee47ec47bed30470181439b52fe6c13b8d2738
oscarzhou <zhouhongyu1989@gmail.com>
2022-04-27 21:41:26 +1200

    chore: update help command output

--- cmd/help.go
+++ cmd/help.go
@@ -9,6 +9,7 @@ Usage: scanreport COMMAND [OPTIONS]
 A tool for analyzing various code security reports (i.e. Snyk, Trivy). Inspired by Github Action Integration
 
 Commands:
+	export	Export the summary/diff report to html file
 	ls		List the files under the specific directory
 	version 	Show the ScanReport version information
 	summary		Return the summary of a security scan report
